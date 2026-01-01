f5ac73e8ca28ce0937ff11ce4ddbfca7180182ec
Lex Ferguson <82519402+SynisterSage@users.noreply.github.com>
2025-12-31 20:52:09 -0500

    fix vite env

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,6 +5,10 @@ interface ImportMetaEnv {
   readonly VITE_STOREFRONT_TOKEN: string;
   readonly VITE_SHOPIFY_API_VERSION: string;
   readonly VITE_USE_MOCKS: string;
+  readonly VITE_CUSTOMER_ACCOUNT_CLIENT_ID: string;
+  readonly VITE_CUSTOMER_ACCOUNT_AUTH_URL: string;
+  readonly VITE_CUSTOMER_ACCOUNT_TOKEN_URL: string;
+  readonly VITE_CUSTOMER_ACCOUNT_LOGOUT_URL: string;
   readonly GEMINI_API_KEY: string;
 }
 
